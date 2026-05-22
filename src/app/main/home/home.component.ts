eb36cf58da1e144a3884a4482d43668dad2726c9
Guest <user@webcontainer.io>
2026-05-22 12:53:20 -0700

    feat: addQuarterly Goals Header implementation

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
 import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
+import { QuarterlyGoalsHeaderComponent } from 'src/app/main/home/quarterly-goals/quarterly-goals-header/quarterly-goals-header.component';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,7 @@ import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
   animations: HomeAnimations,
   imports: [
     NavbarComponent,
+    QuarterlyGoalsHeaderComponent
   ]
 })
 export class HomeComponent implements OnInit {
